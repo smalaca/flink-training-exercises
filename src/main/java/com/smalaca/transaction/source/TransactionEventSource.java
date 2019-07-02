48edec813ffd13ef4a424c3325dff65212b89e93
smalaca <ab13.krakow@gmail.com>
2019-07-02 11:20:04 +0200

    Define "waiting" transaction event source

--- src/main/java/com/smalaca/transaction/source/TransactionEventSource.java
+++ src/main/java/com/smalaca/transaction/source/TransactionEventSource.java
@@ -3,28 +3,29 @@ package com.smalaca.transaction.source;
 import com.smalaca.domain.TransactionEvent;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
+import java.util.Random;
+
 public class TransactionEventSource implements SourceFunction<TransactionEvent> {
     private final RandomTransactionEventFactory factory = new RandomTransactionEventFactory();
 
     @Override
     public void run(SourceContext<TransactionEvent> sourceContext) throws InterruptedException {
-//        while (true) {
-//            Random random = new Random();
-//            int i = random.nextInt(4);
-//
-//
-//        }
-        sourceContext.collect(factory.allocation());
-        sourceContext.collect(factory.allocation());
-        sourceContext.collect(factory.allocation());
-
-        sourceContext.collect(factory.bilateral());
-        sourceContext.collect(factory.bilateral());
-
-        sourceContext.collect(factory.block());
-        sourceContext.collect(factory.block());
+        for(int i = 0; i<100000; i++) {
+            switch (transactionEventType()) {
+                case 0:
+                    sourceContext.collect(factory.allocation());
+                case 1:
+                    sourceContext.collect(factory.bilateral());
+                case 2:
+                    sourceContext.collect(factory.block());
+            }
+        }
 
         Thread.currentThread().join();
+    }
+
+    private int transactionEventType() {
+        return new Random().nextInt(4);
     }
 
     @Override
