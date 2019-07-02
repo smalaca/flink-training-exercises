3b0d98341a05bf2a048c3d55f6f74962e49d292f
smalaca <ab13.krakow@gmail.com>
2019-07-02 11:02:35 +0200

    add explicit configuration

--- src/main/java/com/smalaca/Lab1Job.java
+++ src/main/java/com/smalaca/Lab1Job.java
@@ -3,14 +3,17 @@ package com.smalaca;
 import com.smalaca.domain.ReportFactory;
 import com.smalaca.domain.TransactionEvent;
 import com.smalaca.transaction.source.TransactionEventSource;
+import org.apache.flink.configuration.ConfigConstants;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class Lab1Job {
     public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        env.setParallelism(1);
+        Configuration configuration = new Configuration();
+        configuration.setBoolean(ConfigConstants.LOCAL_START_WEBSERVER, true);
+        int parallelism = 3;
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(parallelism, configuration);
 
         DataStreamSource<TransactionEvent> streamSource = env.addSource(new TransactionEventSource());
         ReportFactory reportFactory = new ReportFactory();
