ac2b275e35974f468dd5d184be915f37fbb266f8
smalaca <ab13.krakow@gmail.com>
2019-07-02 15:06:52 +0200

    Change StreamExecutionEnvironment to use local environment

--- src/main/java/com/smalaca/Lab1Job.java
+++ src/main/java/com/smalaca/Lab1Job.java
@@ -3,16 +3,12 @@ package com.smalaca;
 import com.smalaca.domain.ReportFactory;
 import com.smalaca.domain.TransactionEvent;
 import com.smalaca.transaction.source.TransactionEventSource;
-import org.apache.flink.configuration.ConfigConstants;
-import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class Lab1Job {
     public static void main(String[] args) throws Exception {
-        Configuration configuration = new Configuration();
-        configuration.setBoolean(ConfigConstants.LOCAL_START_WEBSERVER, true);
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
 
         DataStreamSource<TransactionEvent> streamSource = env.addSource(new TransactionEventSource());
         ReportFactory reportFactory = new ReportFactory();
